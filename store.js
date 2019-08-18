9732c36b9da61ad4dcf85778c17a3962781c7ade
Bartłomiej Łoza <bartek.l@lorentus.com>
2019-08-18 19:38:44 +0200

    adds user redux logic

--- store.js
+++ store.js
@@ -4,23 +4,41 @@
 */
 
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 export const actionTypes = {
-  TEST: 'TEST',
+  SAVE_USER_SUCCESS: 'SAVE_USER_SUCCESS',
+  SAVE_USER_ERROR: 'SAVE_USER_ERROR',
+  SAVE_USER_PENDING: 'SAVE_USER_PENDING',
 }
 
 const defaultState = {
-  count: 0
+  user: null,
+  isLoading: false,
+  error: null,
 }
 
 // REDUCER
 export const reducer = (state = defaultState, action) => {
   switch (action.type) {
-    case actionTypes.TEST:
+    case actionTypes.SAVE_USER_SUCCESS:
       return {
-        ...state,
-        count: state.count + 1
+        isLoading: false,
+        user: action.user,
+        error: null
+      }
+    case actionTypes.SAVE_USER_ERROR:
+      return {
+        isLoading: false,
+        user: null,
+        error: action.error
+      }
+    case actionTypes.SAVE_USER_PENDING:
+      return {
+        isLoading: true,
+        user: null,
+        error: null
       }
     default:
       return state
@@ -28,14 +46,34 @@ export const reducer = (state = defaultState, action) => {
 }
 
 // ACTIONS
-export const testACtion = () => {
-  return { type: actionTypes.TEST }
+export const saveUserSuccess = user => {
+  return { type: actionTypes.SAVE_USER_SUCCESS, user }
 }
+
+export const saveUserError = error => {
+  return { type: actionTypes.SAVE_USER_ERROR, error }
+}
+
+export const saveUserPending = () => {
+  return { type: actionTypes.SAVE_USER_PEDNING }
+}
+
+// ASYNC ACTIONS
+export const fetchUserData = user => async dispatch => {
+  dispatch(saveUserPending())
+  const data = await fetch('https://api.github.com/users/Karolloza')
+    .then(res => res.json())
+
+  dispatch(saveUserSuccess(data))
+  // TODO ADD ERROR HANDLING
+}
+
+
 
 export function initializeStore (initialState = defaultState) {
   return createStore(
     reducer,
     initialState,
-    composeWithDevTools(applyMiddleware())
+    composeWithDevTools(applyMiddleware(thunk))
   )
 }
